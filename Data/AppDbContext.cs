74e79ec5d6e140b463f914e733c2d451cc2be9d6
mariyaselvam <mariyaselvam0101@gmail.com>
2025-06-23 17:42:47 +0530

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ECommerceApp.Models.User;
+using ECommerceApp.Models.Product;
+
+namespace ECommerceApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register AppDbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=ecommerce.db")); // or UseSqlServer("...")
+
+var app = builder.Build();
+
+// (Optional) apply migration automatically (dev only)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+app.MapGet("/", () => "E-Commerce App Running");
+
+app.Run();
